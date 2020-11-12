6bb1743d46b18b666b83414c8567c048aa2971e5
Prodip Datta <prodipdatta7@gmail.com>
2020-11-12 19:19:48 +0600

    Update and rename ex2.py to Comments.py

---
+++ Comments.py
@@ -0,0 +1,9 @@
+#Comments
+
+#this is the comment in python
+# everything after # will be ignored in python
+print('This will work')
+# print('but this is not')
+
+#This is to notify that there is no other way of commenting multiple line together in python.
+#In that case, we have to use a single # for every line
--- ex2.py
+++
@@ -1,6 +0,0 @@
-#Comments
-
-#this is the comment in python
-# everything after # will be ignored in python
-print('This will work')
-# print('but this is not')
